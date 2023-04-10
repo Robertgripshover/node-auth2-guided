72e45d18c66307303583e08454cbd7f6307752c1
Robert Gripshover <robertgrips@gmail.com>
2023-04-10 16:02:55 -0400

    m

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,23 @@
+
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../../config')
+
+
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        next({ status: 401, message: `token bad: ${err.message}` })
+      } else {
+        req.decodedJwt = decoded
+        next()
+      } //<< at this point the token validated, we are going to take the deconded token to the req object
+    }) 
+  } else {
+    next({ status: 401, message: 'wat? no token?' })
+  }
 }
 
 // AUTHORIZATION
